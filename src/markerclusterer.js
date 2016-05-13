83fa3fb8462ba81554b7dc7df3d0e3f86d08af7c
David Pelayo <hi@davidpelayo.com>
2016-05-13 12:33:20 +0200

    Updating URL

--- src/markerclusterer.js
+++ src/markerclusterer.js
@@ -188,7 +188,7 @@ function MarkerClusterer(map, opt_markers, opt_options) {
  * @private
  */
 MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ =
-    'https://raw.githubusercontent.com/googlemaps/js-marker-clusterer/gh-pages/' +
+    'https://googlemaps.github.io/js-marker-clusterer/' +
     'images/m';
 
 
